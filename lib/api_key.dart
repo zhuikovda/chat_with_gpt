83dfa1e912677b3bbf185b0cea2848b994cc3fdb
zhuikovda <dmitiy.zhuikov@janix-soft.com>
2024-03-02 00:15:05 +0300

    feat: create chat

---
+++ lib/api_key.dart
@@ -0,0 +1,1 @@
+const API_KEY = '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';
---
+++ lib/chat_screen/chat_screen.dart
@@ -0,0 +1,81 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+
+import '../api_key.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _openAI = OpenAI.instance.build(
+    token: API_KEY,
+    baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
+    enableLog: true,
+  );
+  final ChatUser _user = ChatUser(id: '1');
+  final ChatUser _gpt = ChatUser(id: '2');
+  List<ChatMessage> _messages = [];
+  List<ChatUser> _loadResponse = [];
+
+  Future<void> getResponse(ChatMessage message) async {
+    setState(() {
+      _messages.insert(0, message);
+      _loadResponse.add(_gpt);
+    });
+    List<Map<String, dynamic>> _history = _messages.reversed.map((message) {
+      if (message.user == _user) {
+        return Messages(role: Role.user, content: message.text).toJson();
+      } else {
+        return Messages(role: Role.assistant, content: message.text).toJson();
+      }
+    }).toList();
+    final request = ChatCompleteText(
+      model: GptTurboChatModel(),
+      messages: _history,
+      maxToken: 200,
+    );
+    final response = await _openAI.onChatCompletion(request: request);
+    for (var element in response!.choices) {
+      if (element.message != null) {
+        setState(() {
+          _messages.insert(
+              0,
+              ChatMessage(
+                  user: _gpt,
+                  createdAt: DateTime.now(),
+                  text: element.message!.content));
+        });
+      }
+    }
+    setState(() {
+      _loadResponse.remove(_gpt);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        centerTitle: true,
+        scrolledUnderElevation: 0.0,
+        title: const Text(
+          'Chat with GPT',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: DashChat(
+          typingUsers: _loadResponse,
+          currentUser: _user,
+          onSend: (ChatMessage message) {
+            getResponse(message);
+          },
+          messages: _messages),
+    );
+  }
+}
